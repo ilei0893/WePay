a1cf9a84a749971482d1bd44c6ed8e639ec06f8a
Gisellen <giselle.domingo.x@gmail.com>
2021-10-18 15:51:21 -0400

    Fixed minor bugs from main

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,10 @@
 //comment one or the other to check diff pages
-<<<<<<< HEAD:src/App.js
-import Layout from "./pages/Layout";
-import Home from "./pages/LandingPage/Home";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Employement from "./pages/Dashboard/Employment";
-import AddEmployee from "./pages/Dashboard/AddEmployee";
-import Inbox from "./pages/Dashboard/Inbox";
-=======
 import Layout from "../pages/Layout";
 import Home from "../pages/LandingPage/Home";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Employement from "../pages/Dashboard/Employment";
+import AddEmployee from "../pages/Dashboard/AddEmployee";
 import Inbox from "../pages/Dashboard/Inbox";
->>>>>>> c5a2046a2bf6cb6d40d4a47e798136f7fd4e87c9:src/app/App.js
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
