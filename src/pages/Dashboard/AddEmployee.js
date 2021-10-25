40682a3ba12a57d49e750c39995e1ed9db418105
Gisellen <giselle.domingo.x@gmail.com>
2021-10-25 15:00:14 -0400

    Added the rest of the inputs for the form

--- src/pages/Dashboard/AddEmployee.js
+++ src/pages/Dashboard/AddEmployee.js
@@ -21,9 +21,33 @@ export default class AddEmployee extends Component {
                 <Form.Label>Last Name</Form.Label>
                 <Form.Control type="text" placeholder="Last Name" />
               </Form.Group>
+              <Form.Group className="mb-3" controlId="formSSN">
+                <Form.Label>SSN</Form.Label>
+                <Form.Control type="text" placeholder="xxx xx xxxx" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formAddress">
+                <Form.Label>Address</Form.Label>
+                <Form.Control type="text" placeholder="00 Smiths Place" />
+              </Form.Group>
               <Form.Group className="mb-3" controlId="formPhoneNumber">
                 <Form.Label>Phone Number</Form.Label>
                 <Form.Control type="tel" placeholder="(xxx)-xxx-xxxx" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formSalary">
+                <Form.Label>Salary</Form.Label>
+                <Form.Control type="text" placeholder="$50,000" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formPosition">
+                <Form.Label>Position</Form.Label>
+                <Form.Control type="text" placeholder="$50,000" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formWorkState">
+                <Form.Label>Work State</Form.Label>
+                <Form.Control type="text" placeholder="NY" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formLivingState">
+                <Form.Label>Living State</Form.Label>
+                <Form.Control type="text" placeholder="NY" />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Submit
